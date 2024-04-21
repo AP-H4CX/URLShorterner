48e3d87b4428f04596798564d47696f4a491b6ea
Aditya Kumar <146692549+AP-H4CX@users.noreply.github.com>
2024-04-21 07:11:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+API_ID = int(os.getenv("API_ID", "23007799"))
+API_HASH = os.environ.get("API_HASH", "aa694ed52dbd072d6384053560132057")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6654040372:AAFKq29o0lChuii9cnRzTs7g9QsV63OlkaM")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS", "").split(",") if i.strip()] 
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
 DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "6036777486")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002084501753")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Moviesflix44") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
 LINK_BYPASS = "False"
